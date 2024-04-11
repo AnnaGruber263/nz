d533b2bd0c3b77b8ede74ba323758db377ec5b10
AnnaGruber263 <anna.gruber@uibk.ac.at>
2024-04-11 18:26:09 +0200

    Etappenseite verändert

--- main.js
+++ main.js
@@ -300,7 +300,7 @@ L.geoJSON(jsondata, {}).bindPopup(function (layer) {
     <ul>
     <li>Breite: ${layer.feature.geometry.coordinates[1].toFixed(5)} </li>
     <li>Länge: ${layer.feature.geometry.coordinates[0].toFixed(5)} </li>
-    <li><a href="https://${layer.feature.properties.user}.github.io/nz">Etappenseite>/
+    <li><a href="https://${layer.feature.properties.user}.github.io/nz">Etappenseite</
     a></li>
     <li><a href="${layer.feature.properties.wikipedia}">Wikipediaseite</li>
 
